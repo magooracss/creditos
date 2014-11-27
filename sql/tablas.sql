b30c9178b95badfd65121d27a957aec42f35c694
Carlos Enrique Rodriguez <lw4dbf@gmail.com>
2014-11-27 10:22:02 -0300

    SQL para manejar los datos de afiliados

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -33,3 +33,61 @@ CREATE TABLE Accesos
     , accesoID      "guid"  DEFAULT '{00000000-0000-0000-0000-000000000000}'
     , grupo_id      "guid"  DEFAULT '{00000000-0000-0000-0000-000000000000}'
 );
+
+CREATE TABLE Afiliados
+(
+      id   		    "guid"  NOT NULL PRIMARY KEY
+    , apellidos     varchar(250)
+    , nombres       varchar(250)
+    , documentoTipo_id  integer default 0
+    , documento     varchar(30)
+    , cuit          varchar(15)
+    , domicilio     varchar(300)
+    , localidad_id  integer default 0
+    , Notas         blob
+    , bVisible      smallint default 1
+);
+
+CREATE TABLE AfiliadosEmpresas
+(
+      id   		    "guid"  NOT NULL PRIMARY KEY
+    , afiliado_id    "guid"  DEFAULT '{00000000-0000-0000-0000-000000000000}'
+    , empresa_id    "guid"  DEFAULT '{00000000-0000-0000-0000-000000000000}'
+    , reparticion_id    integer default 0
+    , nroAfiliado       integer default -1
+    , legajo            varchar(40)
+    , banco_id          integer default 0
+    , bVisible      smallint default 1
+);
+
+CREATE TABLE DocumentosTipos
+(
+     id  integer NOT NULL PRIMARY KEY
+   , tipo   varchar(25)
+   , bVisible smallint default 1
+);
+
+CREATE TABLE Localidades
+(
+     id integer NOT NULL PRIMARY KEY
+   , localidad  varchar(150)
+   , codigoPostal   varchar(10)
+   , bVisible smallint default 1
+);
+
+CREATE TABLE Reparticiones
+(
+     id integer NOT NULL PRIMARY KEY
+   , reparticion  varchar(250)
+   , codigo   varchar(20)
+   , bVisible smallint default 1
+);
+
+CREATE TABLE Bancos
+(
+     id integer NOT NULL PRIMARY KEY
+   , Banco  varchar(250)
+   , Codigo varchar(25)
+   , PrestacionBapro smallint default 0
+   , bVisible smallint default 1
+);
---
+++ sql/triggers.sql
@@ -0,0 +1,47 @@
+SET TERM ^ ;
+
+CREATE TRIGGER documentoTipoID FOR DocumentosTipos
+BEFORE INSERT POSITION 0
+AS 
+BEGIN 
+    If (New.id = -1) then
+   New.id = GEN_ID(documentoTipoID,1);
+END^
+
+SET TERM ; ^  
+
+SET TERM ^ ;
+
+CREATE TRIGGER localidadID FOR Localidades
+BEFORE INSERT POSITION 0
+AS 
+BEGIN 
+    If (New.id = -1) then
+   New.id = GEN_ID(localidadID,1);
+END^
+
+SET TERM ; ^  
+
+SET TERM ^ ;
+
+CREATE TRIGGER reparticionID FOR Reparticiones
+BEFORE INSERT POSITION 0
+AS 
+BEGIN 
+    If (New.id = -1) then
+   New.id = GEN_ID(reparticionID,1);
+END^
+
+SET TERM ; ^  
+
+SET TERM ^ ;
+
+CREATE TRIGGER bancoID FOR Bancos
+BEFORE INSERT POSITION 0
+AS 
+BEGIN 
+    If (New.id = -1) then
+   New.id = GEN_ID(bancoID,1);
+END^
+
+SET TERM ; ^  
